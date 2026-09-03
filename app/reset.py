468b991c5c5c54e265953e079b305fd17d76b7f3
Seoha101109 <122782179+ikiterujan@users.noreply.github.com>
2026-09-03 11:53:57 +0900

    강제삭제

--- app/reset.py
+++ app/reset.py
@@ -3,5 +3,5 @@ from app.core.database import Base, engine
 
 def resetdb():
     # 모든 테이블 삭제 후 다시 생성
-    Base.metadata.drop_all(bind=engine)
+    Base.metadata.drop_all(bind=engine, oracle_drop_cascade=True)
     Base.metadata.create_all(bind=engine)
