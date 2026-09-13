6a0be6ad6940d1cad36051a64d7ebb51b3bca2c6
Seoha101109 <122782179+ikiterujan@users.noreply.github.com>
2026-09-13 09:04:16 +0900

    Update scheduler.py

--- app/tasks/scheduler.py
+++ app/tasks/scheduler.py
@@ -64,8 +64,8 @@ def start_scheduler():
     scheduler.add_job(
         send_daily_notice_task,
         "cron",
-        hour=8,
-        minute=55,
+        hour=9,
+        minute=10,
         id="daily_notice_job",
         replace_existing=True
     )
